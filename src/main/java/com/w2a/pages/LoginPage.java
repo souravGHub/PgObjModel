276c6a1927d0be585dead595633895a46f8510bc
sourav <sourav2u06@gmail.com>
2020-06-22 12:58:30 -0400

    Jenkins build through git

--- src/main/java/com/w2a/pages/LoginPage.java
+++ src/main/java/com/w2a/pages/LoginPage.java
@@ -2,6 +2,7 @@ package com.w2a.pages;
 import com.w2a.base.Page;
 public class LoginPage extends Page {
 	public ZohoAppPage doLogin(String userName, String password) {
+		System.out.println("Start login test");
 		type("userid_XPATH", userName);
 		click("signinButton_XPATH");
 		type("password_XPATH", password);
